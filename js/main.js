e3ef4515991166ff82cfeb6d1ddca84c6b262ef1
Francis Githae <francis.githae@student.moringaschool.com>
2021-11-14 16:22:13 +0300

    add logic to calculate day of the week born

--- js/main.js
+++ js/main.js
@@ -1,6 +1,47 @@
-$('.header-section').height($(window).height());
+$(".header-section").height($(window).height());
 
-let femaleAkan={
-  Sunday:"Kwasi",
-  Monday:""
-}
+let femaleAkan = {
+  Sunday: "Kwasi",
+  Monday: "Kwadwo",
+  Tuesday: "Kwabena",
+  Wednesday: "Kwaku",
+  Thursday: "Yaw",
+  Friday: "Kofi",
+  Saturday: "Kwame",
+};
+let maleAkan = {
+  Sunday: "Akosua",
+  Monday: "Adwoa",
+  Tuesday: "Abenaa",
+  Wednesday: "Akua",
+  Thursday: "Yaa",
+  Friday: "Afua",
+  Saturday: "Ama",
+};
+
+let akanNames = [femaleAkan, maleAkan];
+let day=16;
+let month=12;
+let year=1996;
+
+let CC=19;
+let MM=1;
+let YY=75;
+let DD=15; 
+let dayOfTheWeek=(((CC/4)-2*CC-1)+(Math.floor(5*YY/4))+(Math.floor(26*(MM+1)/10))+DD)%7;
+console.log(dayOfTheWeek);
+
+document.addEventListener('DOMContentLoaded',()=>{
+
+  
+})
+
+// let k=16;
+// let m=10;
+// let C=19;
+// let Y=96;
+
+// let dayOfTheWeek2=(k + Math.floor(2.6*m-0.2)-2*C + Y + Math.floor(Y/4))%7
+
+
+// console.log(dayOfTheWeek2)
